4ebf6f0bf6140da81a73a064e73bd4b516fda81c
Eugenio Pacelli Flores <flores.eugenio03@gmail.com>
2023-01-01 11:11:10 -0500

    modified config

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,30 +4,12 @@ require("dotenv").config()
 const MAINNET_RPC_URL =
     process.env.ALCHEMY_MAINNET_RPC_URL ||
     "https://eth-mainnet.alchemyapi.io/v2/your-api-key"
-const GOERLI_RPC_URL =
-    process.env.GOERLI_RPC_URL ||
-    "https://eth-goerli.alchemyapi.io/v2/your-api-key"
-const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
     networks: {
-        hardhat: {
-            chainId: 31337,
-        },
-        localhost: {
-            chainId: 31337,
-        },
-        goerli: {
-            url: GOERLI_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            chainId: 5,
-        },
         mainnet: {
             url: MAINNET_RPC_URL,
-            accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
-            chainId: 1,
         },
     },
-    defaultNetwork: "hardhat",
 }
